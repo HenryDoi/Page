d1724050ca42ad9fd1c8ee9f39fbe1b0a292ffae
HenryDoi <89016656+HenryDoi@users.noreply.github.com>
2021-08-18 20:28:47 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-var numOne = document.getElementById("num_one");
-var numTwo = document.getElementById("num_two");
-var addSum = document.getElementById("add_sum");
-
-numOne.addEventListener("input", add);
-numTwo.addEventListener("input", add);
-
-function add(){
-    var one = parseFloat(numOne.value) || 0;
-    var two = parseFloat(numTwo.value) || 0;
-    addSum.innerHTML = "相加得：" + (one + two);
-}
+console.log('Hello World')
